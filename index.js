c2a2c2b72d38abcd91bc39aa49f641fe7bb0e93c
PratyushG434 <pratyush1534@gmail.com>
2025-02-09 23:18:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import bodyParser from 'body-parser';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import weatherAndNewsRoutes from './routes/weatherAndNewsRoutes.js';
-
-// import redisClient from './config/redis.js';
+import redisClient from './config/redis.js';
 
 const app = express();
 const port = 3000;
@@ -13,10 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connecting to Redis
-// (async () => {
-//     await redisClient.connect();
-//     console.log("Connected to Redis");
-// })();
+(async () => {
+    await redisClient.connect();
+    console.log("Connected to Redis");
+})();
 
 // Routes
 app.use('/auth', authRoutes);
